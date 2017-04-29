2900060b8ade7ed5956fa90d4cdd68c32ad34119
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2017-04-29 17:00:56 +0800

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,47 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <b>Angular 2 Component using Observables!</b>
+	  
+	  <h5 style="margin-bottom: 0">VALUES</h5>
+	  <div>{{ values.toString() }}</div>
+	  
+	  <h5 style="margin-bottom: 0">ERRORS</h5>
+	  <pre><code>{{anyErrors}}</code></pre>
+	  
+	  <button style="margin-top: 2rem;" (click)="init()">Init</button>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+
+  private data: Observable<Array<number>>;
+	private values: Array<number> = [];
+	private anyErrors: Error;
+
+  init() {
+
+		this.data = new Observable(observer => 
+		  {
+			setTimeout(() => {
+				observer.error('Hey something bad happened I guess');
+			}, 2000);
+			{
+			  for(var i = 0; i<10; i++)
+			  {
+			    console.log(i);
+			  }
+			}
+			{
+			  console.log('sample');
+			}
+		});
+
+		let subscription = this.data.subscribe(
+			//error => this.anyErrors = error
+		);
+
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import 'rxjs/Rx';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
